739eaa7e839bcdc5aba5e46bb190edccaa1fbb30
JNGDSOFT <jngd.public@gmail.com>
2025-12-17 14:06:58 -0500

    valid user authentication is succesfully working

--- src/main/java/com/music_academy/app/infrastructure/persistance/CustomUserDetailsServiceAdapter.java
+++ src/main/java/com/music_academy/app/infrastructure/persistance/CustomUserDetailsServiceAdapter.java
@@ -3,11 +3,19 @@ package com.music_academy.app.infrastructure.persistance;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-public class CustomUserDetailsServiceAdapter implements UserDetailsService{
+import com.music_academy.app.infrastructure.exception.NotFoundByNameException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailsServiceAdapter implements UserDetailsService {
+	private final SpringDataUserRepository springDataUserRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		return springDataUserRepository.findByEmail(username).orElseThrow(NotFoundByNameException::new);
 	}
 }
