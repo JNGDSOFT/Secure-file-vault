991e9f4eeb1852ea263014b3c7055364340f00b0
JNGDSOFT <jngd.public@gmail.com>
2025-12-17 11:56:49 -0500

    an endpoint for authentication testing was created, and userdetailsservice implementations was succeed by a new customUserDetailsService adapter

--- src/main/java/com/music_academy/app/infrastructure/controller/HelloWorldController.java
+++ src/main/java/com/music_academy/app/infrastructure/controller/HelloWorldController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/helloworld")
 @RestController
 public class HelloWorldController {
-	
+
 	@GetMapping
 	public String helloWorld() {
 		return "Hello world!";
 	}
-	
+
+	@GetMapping("/auth")
+	public String helloWorldAuthenticated() {
+		return "Hello world! ... authenticated!";
+	}
+
 }
