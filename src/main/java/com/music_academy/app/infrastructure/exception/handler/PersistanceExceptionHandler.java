6f35135e8c282af8c1d9c41eaa4c28a06ec73c7f
JNGDSOFT <jngd.public@gmail.com>
2025-12-18 15:39:05 -0500

    duplicity exception is now being managed by generic exception response

--- src/main/java/com/music_academy/app/infrastructure/exception/handler/PersistanceExceptionHandler.java
+++ src/main/java/com/music_academy/app/infrastructure/exception/handler/PersistanceExceptionHandler.java
@@ -7,16 +7,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.music_academy.app.infrastructure.exception.handler.model.GenericExceptionResponseBody;
+
 @RestControllerAdvice
 public class PersistanceExceptionHandler {
 	@ExceptionHandler(exception = SQLException.class)
-	ResponseEntity<String> test(SQLException sqlException) {
+	ResponseEntity<GenericExceptionResponseBody> test(SQLException sqlException) {
 		String sqlState = sqlException.getSQLState();
 		int code = Integer.parseInt(sqlState);
+		String error = "";
 		String message = "";
 		if (code == 23505) {
+			error = "duplicity";
 			message = "That email already exists";
 		}
-		return ResponseEntity.status(HttpStatusCode.valueOf(409)).body(message);
+		return ResponseEntity.status(HttpStatusCode.valueOf(409))
+				.body(GenericExceptionResponseBody.builder().error(error).message(message).build());
 	}
 }
