4fb568bb31be3b8cf0121334e07a7970370a2360
JNGDSOFT <jngd.public@gmail.com>
2025-12-29 13:44:53 -0500

    user owner authenticity and node type validations are done. Including the change to nullable parent id in folder creation request dto

--- src/main/java/com/music_academy/app/infrastructure/controller/dto/CreateFolderRequestDTO.java
+++ src/main/java/com/music_academy/app/infrastructure/controller/dto/CreateFolderRequestDTO.java
@@ -4,8 +4,7 @@ import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CreateFolderRequestDTO(
-		@NotBlank(message = "There is no any parent uuid. You can´t create a second root node") UUID parentNode,
+public record CreateFolderRequestDTO(UUID parentNode,
 		@NotBlank(message = "you have to set a name for the new folder") String name) {
 
 }
